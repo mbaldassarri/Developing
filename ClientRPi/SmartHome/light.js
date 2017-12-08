1c62545b44c59f739f47efc55aed59f8931e9cbb
Marco Baldassarri <marcobaldassarri@mangomobi.com>
2017-12-08 19:47:52 +0100

    deleted references to mqtt - not used in this branch

--- ClientRPi/SmartHome/light.js
+++ ClientRPi/SmartHome/light.js
@@ -1,35 +1,9 @@
-const mqtt = require('mqtt');
 var Gpio = require('onoff').Gpio;
 var lamp = new Gpio(18, 'high');
 const vars = require('./vars');
 const dash_button = require('node-dash-button');
 var dash = dash_button([vars.AMAZON_DASH_ALERT_MAC, vars.AMAZON_DASH_IOT_MAC], null, null, 'all'); 
 
-const client = mqtt.connect('http://insert-mqtt-broker-address-here:8883', {username: 'username', password: 'password'})
-
-client.on('connect', function () {
-  client.subscribe('smarthome/light')
-  console.log("OK CONNECTED")
-})
-
-client.on('message', function (topic, message) {
-  if(topic == 'smarthome/light'){
-	console.log(message.toString());
-    if(message == '1'){
-      lamp.writeSync(0);
-    }
-    if(message == '0'){
-      lamp.writeSync(1);
-    }
-  }
-  console.log(message.toString())
-})
-
-
-process.on('SIGINT', function () {
-  lamp.unexport();
-  client.end()
-});
 
 dash.on("detected", function (dash_id){
   if (dash_id === vars.AMAZON_DASH_IOT_MAC){
