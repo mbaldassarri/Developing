51a99ea2c54119d05d4dcee594c96e8c88d4602b
Marco Baldassarri <marcobaldassarri@mangomobi.com>
2017-12-08 19:13:34 +0100

    add dash button node integration

--- ClientRPi/SmartHome/light.js
+++ ClientRPi/SmartHome/light.js
@@ -1,6 +1,9 @@
 const mqtt = require('mqtt');
 var Gpio = require('onoff').Gpio;
 var lamp = new Gpio(18, 'high');
+const vars = require('./vars');
+const dash_button = require('node-dash-button');
+var dash = dash_button([vars.AMAZON_DASH_ALERT_MAC, vars.AMAZON_DASH_IOT_MAC], null, null, 'all'); 
 
 const client = mqtt.connect('http://insert-mqtt-broker-address-here:8883', {username: 'username', password: 'password'})
 
@@ -26,4 +29,25 @@ client.on('message', function (topic, message) {
 process.on('SIGINT', function () {
   lamp.unexport();
   client.end()
+});
+
+dash.on("detected", function (dash_id){
+  if (dash_id === vars.AMAZON_DASH_IOT_MAC){
+    lamp.read(status =>
+      (status === '0') ? lamp.writeSync(1) : lamp.writeSync(0));
+      //type http post call to localhost Home-Assistant here to turn on lights
+  }
+});
+
+bot.on('/hello', (msg) => {
+  return bot.sendMessage(msg.from.id, `Ciao ${ msg.from.first_name }! Ti trovo bene oggi ! \u{1F604}`);
+});
+
+bot.on('/accendi_luci', (msg) => {
+  //http post call to localhost HA to turn on lights
+  bot.sendMessage(msg.from.id, `${ msg.from.first_name }, ho acceso le luci !`);
+});
+
+bot.on('/spegni_luci', (msg) => {
+  bot.sendMessage(msg.from.id, `${ msg.from.first_name }, ho spento le luci !`);
 });
---
+++ ClientRPi/SmartHome/vars.js
@@ -0,0 +1,2 @@
+exports.TELEGRAM_BOT_TOKEN = "404750815:AAGC4D0mNnjzrNk5P2hSoDNXMR3YStvjzh4";
+exports.AMAZON_DASH_IOT_MAC = "ac:63:be:78:b1:2e";
