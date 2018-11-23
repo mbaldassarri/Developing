4734376610bbbc6e21064385f827158f80e5930a
Marco Baldassarri <marcobaldassarri@mangomobi.com>
2018-11-23 18:14:17 +0100

    * rPi Client: Add topic status to get the current status from server * rPi Client: change status based on current status to make the script stateless

--- clients/raspberrypi/light.js
+++ clients/raspberrypi/light.js
@@ -4,25 +4,24 @@ var lamp = new Gpio(14, 'high');
 var mqtt = require('mqtt'), url = require('url');
 var mqtt_url = url.parse("mqtt://zbqlnwjk:bF4CAJfM5AGI@m15.cloudmqtt.com:12505");
 const client = mqtt.connect(mqtt_url)
+const topic_current_status = 'smarthome/status';
+const topic_change_status = 'smarthome/light';
 
 client.on('connect', function () {
   client.subscribe('smarthome/light')
-  console.log("OK CONNECTED")
+  console.log('Current status: ', lamp.readSync() == 0 ? 'OFF' : 'ON');
 })
 
 client.on('message', function (topic, message) {
-  
-  if(topic == 'smarthome/light'){
-    console.log(message.toString());
-    if(message == '0'){
-      lamp.writeSync(0);
-    }
-    if(message == '1'){
-      lamp.writeSync(1);
-    }
+  if(topic == topic_current_status) {
+    client.publish('smarthome/status', lamp.readSync())
+  }
+  if(topic == topic_change_status){
+    if(message != lamp.readSync()){
+      lamp.writeSync(lamp.readSync() ^ 1)
+    } 
   }
 })
-
 
 process.on('SIGINT', function () {
   lamp.unexport();
