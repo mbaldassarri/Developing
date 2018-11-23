b88b4ab78ed1f074256d435ceaed626c345580a0
Marco Baldassarri <marcobaldassarri@mangomobi.com>
2018-11-24 00:08:06 +0100

    *mqtt refactoring in server and rPi client to make it stateless

--- Server/routes/light.js
+++ Server/routes/light.js
@@ -1,35 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var mqtt = require('mqtt'), url = require('url');
-var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1884');
-
+var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
+var currentStatus = '';
+const topic_status_current = 'smarthome/status';
+const topic_status_change = 'smarthome/light';
 const client = mqtt.connect(mqtt_url)
 
 client.on('connect', function () {
-  client.subscribe('smarthome/light')
-  console.log("MQTT CONNECTED")
+  client.subscribe(topic_status_current)
+  client.subscribe(topic_status_change)
 })
 
-var status;
-router.post('/', function(req, res, next) {
-  if(req.body.status == 'off') {
-    status = "0";
-    client.publish('smarthome/light', status)
-    
-    res.send({status: status})
-  } else if(req.body.status == 'on'){
-    status = "1";
-    client.publish('smarthome/light', status)
-    
-    res.send({status: status})
+client.on('message', function (topic, message) {
+  if(topic == topic_status_current) {
+    currentStatus = message.toString();
   }
+})
+
+router.post('/', function(req, res) {
+  if(req.body.status != currentStatus) {
+    client.publish(topic_status_change, req.body.status)
+  } 
+  res.send({status: currentStatus})
 });
 
-
 router.get('/', function(req, res){
-  res.send({status: status});
+  res.send({status: currentStatus});
 })
-
 
 module.exports = router;
 
--- clients/raspberrypi/light.js
+++ clients/raspberrypi/light.js
@@ -4,21 +4,25 @@ var lamp = new Gpio(14, 'high');
 var mqtt = require('mqtt'), url = require('url');
 var mqtt_url = url.parse("mqtt://zbqlnwjk:bF4CAJfM5AGI@m15.cloudmqtt.com:12505");
 const client = mqtt.connect(mqtt_url)
-const topic_current_status = 'smarthome/status';
-const topic_change_status = 'smarthome/light';
+const topic_status_current = 'smarthome/status';
+const topic_status_change = 'smarthome/light';
 
 client.on('connect', function () {
-  client.subscribe('smarthome/light')
+  client.subscribe(topic_status_current)
+  client.subscribe(topic_status_change, function (err) {
+    if (!err) {
+      client.publish(topic_status_current, lamp.readSync().toString())
+    }
+  })
   console.log('Current status: ', lamp.readSync() == 0 ? 'OFF' : 'ON');
 })
 
 client.on('message', function (topic, message) {
-  if(topic == topic_current_status) {
-    client.publish('smarthome/status', lamp.readSync())
-  }
-  if(topic == topic_change_status){
+  if(topic == topic_status_change){
     if(message != lamp.readSync()){
-      lamp.writeSync(lamp.readSync() ^ 1)
+      var newStatus = (lamp.readSync() ^ 1);
+      lamp.writeSync(newStatus)
+      client.publish(topic_status_current, newStatus.toString())
     } 
   }
 })
