826b0a766af6c3930e952248c83953e13425f63f
Marco Baldassarri <marcobaldassarri@mangomobi.com>
2018-12-17 22:48:28 +0100

    adding amazon-dash support to turn on/off the light

--- clients/raspberrypi/light.js
+++ clients/raspberrypi/light.js
@@ -1,11 +1,13 @@
 var Gpio = require('onoff').Gpio;
 var lamp = new Gpio(14, 'high');
-
+var dash_button = require('node-dash-button');
 var mqtt = require('mqtt'), url = require('url');
 var mqtt_url = url.parse("mqtt://zbqlnwjk:bF4CAJfM5AGI@m15.cloudmqtt.com:12505");
 const client = mqtt.connect(mqtt_url)
 const topic_status_current = 'smarthome/status';
 const topic_status_change = 'smarthome/light';
+const vars = require('./vars');
+var dash = dash_button(vars.AMAZON_DASH_ALERT_MAC, null, null, 'all'); 
 
 client.on('connect', function () {
   client.subscribe(topic_status_current)
@@ -26,6 +28,13 @@ client.on('message', function (topic, message) {
     } 
   }
 })
+
+dash.on("detected", function (dash_id){
+  if (dash_id === vars.AMAZON_DASH_ALERT_MAC){
+    var newStatus = (lamp.readSync() ^ 1);
+    lamp.writeSync(newStatus)
+  }
+});
 
 process.on('SIGINT', function () {
   lamp.unexport();
---
+++ clients/raspberrypi/vars.js
@@ -0,0 +1,1 @@
+exports.AMAZON_DASH_ALERT_MAC = "00:fc:8b:54:34:1c";
